d140550b7d8998ccfb39b129b1059afd32914f6d
yumoldianne <dianne.yumol@student.ateneo.edu>
2024-07-28 19:55:45 +0800

    Updated pages

--- Home.py
+++ Home.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from st_pages import add_page_title, get_nav_from_toml, hide_pages
+from st_pages import hide_pages
 
 st.set_page_config(
     page_title="Home",
@@ -7,12 +7,6 @@ st.set_page_config(
     layout="wide",
     initial_sidebar_state="expanded"
 )
-
-nav = get_nav_from_toml(".streamlit/pages.toml")
-
-pg = st.navigation(nav)
-
-add_page_title(pg)
 
 primaryColor = "#575fe8"
 backgroundColor = "#f5f7f3"
