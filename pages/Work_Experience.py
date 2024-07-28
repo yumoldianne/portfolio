ea50ed2a5cd4db986ecfe4fa04e54fb38089093a
yumoldianne <dianne.yumol@student.ateneo.edu>
2024-07-28 19:34:55 +0800

    Updated pages

--- pages/Work_Experience.py
+++ pages/Work_Experience.py
@@ -3,7 +3,7 @@ from st_pages import add_page_title, get_nav_from_toml, hide_pages
 
 st.set_page_config(layout="wide")
 
-nav = get_nav_from_toml(".streamlit/pages_sections.toml")
+nav = get_nav_from_toml(".streamlit/pages.toml")
 
 pg = st.navigation(nav)
 
