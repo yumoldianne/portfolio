3fe6d61c58dce36a2ea4bcae2ca853a5d4335d5f
yumoldianne <dianne.yumol@student.ateneo.edu>
2024-07-28 19:40:08 +0800

    Updated config

--- pages/Work_Experience.py
+++ pages/Work_Experience.py
@@ -1,8 +1,6 @@
 import streamlit as st
 from st_pages import add_page_title, get_nav_from_toml, hide_pages
 
-st.set_page_config(layout="wide")
-
 nav = get_nav_from_toml(".streamlit/pages.toml")
 
 pg = st.navigation(nav)
